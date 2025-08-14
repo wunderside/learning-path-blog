945a5269e23297798b9e61d6d7c8316d6f7756bb
turo-webdev <158546249+turo-webdev@users.noreply.github.com>
2025-08-13 22:56:20 -0600

    Switch from salite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_08_13_210226) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
